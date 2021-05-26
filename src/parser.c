2c82dc84bf5f495eb0f8f7856d2760f2a9327680
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-05-26 09:37:46 -0400

    parser inital

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,7 @@
 #include "../include/parser.h"
 #include <stdlib.h>
 
-ParserStatus parser_start(TokenList* list, const char* source)
+ParserStatus parser_start(TokenList *list, const char *source)
 {
     char lex[256];
     int lexi = 0;
@@ -16,29 +16,36 @@ ParserStatus parser_start(TokenList* list, const char* source)
         }
         lex[lexi] = '\0';
 
-        if(lex[0] == '\n'){
+        if (lex[0] == '\0')
             break;
-        }
-        //CREATION OF A NUMERICAL SENTENCE
-        if(lex[0] == '#'){
-            int num = parser_get_number(lex);
-            printf("NUMBER: %d\n",num);      
-            token_list_add(list, token_create(NUMBER, num, line));
-        }
-        else{
-            int inst = parser_get_inst(lex);
-            if(inst >= 0){
-                printf("INST: %s\n",lex);
-                   token_list_add(list, token_create(INST, inst, line));
-            }else{
-                printf("Syntax error: no such instruction '%s'\n",lex);
-                return PARSER_SYNTAX_ERROR;
+      
+            //CREATION OF A NUMERICAL SENTENCE
+            if (lex[0] == '#')
+            {
+                int num = parser_get_number(lex);
+                printf("NUMBER: %d\n", num);
+                token_list_add(list, token_create(NUMBER, num, line));
             }
+            else
+            {
+                int inst = parser_get_inst(lex);
+                if (inst >= 0)
+                {
+                    printf("INST: %s\n", lex);
+                    token_list_add(list, token_create(INST, inst, line));
+                }
+                else
+                {
+                    printf("Syntax error: no such instruction '%s'\n", lex);
+                    return PARSER_SYNTAX_ERROR;
+                }
+            }
+        if (source[i] == '\n')
+        {
+            line++;
         }
-        if(source[i] == '\n'){
-            line++;  
-        }
-        else if(source[i] == '\0'){
+        else if (source[i] == '\0')
+        {
             break;
         }
 
@@ -47,16 +54,24 @@ ParserStatus parser_start(TokenList* list, const char* source)
     }
     return PARSER_SUCCESS;
 }
-uint32_t parser_get_number(const char* buf){
+uint32_t parser_get_number(const char *buf)
+{
     long num = atoi(&buf[1]);
     return (num <= UINT32_MAX) ? num : 0;
 }
-TokenInst parser_get_inst(const char* buf){
-    if(strcmp(buf, "push") == 0)
+TokenInst parser_get_inst(const char *buf)
+{
+    if (strcmp(buf, "push") == 0)
+    {
         return PUSH;
-    if(strcmp(buf, "add") == 0)
-        return ADD;
-    if(strcmp(buf, "hlt") == 0)
-        return HLT;
-    return (TokenInst) -1;
     }
+    if (strcmp(buf, "hlt") == 0)
+    {
+        return HLT;
+    }
+    if (strcmp(buf, "add") == 0)
+    {
+        return ADD;
+    }
+    return (TokenInst)-1;
+}
